1151bccde0e17fd2698d4037adc28dd812a2fcaf
slee00 <slee000220@gmail.com>
2022-01-03 16:24:52 +0900

    convert

---
+++ Conversion/Conversion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "50000";
+            int n;
+            n = Convert.ToInt32(s); //명시적 형 변환
+            Console.WriteLine("{0}", s);
+            Console.WriteLine("{0}", n);
+        }
+    }
+}
