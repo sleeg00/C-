92bd20264aa3a093b8db1f0e2ec88cf0433115a4
slee00 <slee000220@gmail.com>
2022-01-04 17:17:21 +0900

    이차원배열

---
+++ Two/Two/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[3, 5];
+            int k = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    k++;
+                    a[i, j] = k;
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(" {0} ",a[i, j]);
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                    Console.WriteLine("{0},{1} : {2}", i, j, a[i, j]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
