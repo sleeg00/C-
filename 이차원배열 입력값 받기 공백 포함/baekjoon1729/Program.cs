6cf172fb3a99c88435ff18a992a9d96a53cc68c1
slee00 <slee000220@gmail.com>
2022-01-05 16:10:50 +0900

    이차원배열1

---
+++ 이차원배열 입력값 받기 공백 포함/baekjoon1729/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace baekjoon1729
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[10,10];
+            for(int i=0; i<3; i++)
+            { 
+                for(int j=0; j<6; j++)
+                {
+                    string input=Console.ReadLine();
+                    int k=Convert.ToInt32(input);
+                    a[i, j] = k;
+                }
+            }
+               
+            for(int i=0; i<3; i++)
+            {
+                for(int j=0; j<6; j++)
+                    Console.Write(a[i,j]);
+                Console.WriteLine();
+            }
+                
+        }
+    }
+}
