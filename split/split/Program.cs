a26957d45507139345100977215f2eef5a02a02c
slee00 <slee000220@gmail.com>
2022-01-05 21:02:41 +0900

    split

---
+++ split/split/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int max = 3;
+			int[,] a = new int[max, max];
+			string s;
+			int i = 0;
+			while ((s = Console.ReadLine()) != null)
+			{
+				string[] split = s.Split(" "); /// Split이란 ("ex")ex를 제외해줌 예외처리
+				///split배열에 증가 결국 1차원베열에 넣어서 2차원배열에 넣는다
+				for (int j = 0; j < split.Length; ++j)
+					a[i, j] = int.Parse(split[j]);
+				if (++i == max)
+					break;
+			}
+		}
+    }
+}
