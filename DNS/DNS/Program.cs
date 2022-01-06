4c7f83bcd61d8c717b36992a6b07be355a2df8e2
slee00 <slee000220@gmail.com>
2022-01-06 17:56:46 +0900

    Dns사용법

---
+++ DNS/DNS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;///dns 사용
+
+namespace DNS
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            string mycomputer = Dns.GetHostName ();
+            ///Dns 이름 넣기 naver,gogle
+            Console.WriteLine("computer name : " + mycomputer);
+
+            IPHostEntry entry= Dns.GetHostEntry("www.naver.com");
+            /// IPHostEnty는 int char string 변수형 Dns를 보관하기 위한
+            /// GetHostEntry는 ip주소를 가져옴
+            foreach(IPAddress ipAddress in entry.AddressList)///foreach는 끝날떄까지 집어넣음
+            {
+                ///break가 설정되있는 while?
+                Console.Write("ip of www.naver.com is : ");
+                Console.WriteLine(ipAddress);
+            }
+        }
+    }
+}
