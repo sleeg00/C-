022efed75f14de652b091693e9fa0a53c34c1397
slee00 <slee000220@gmail.com>
2022-01-11 19:41:33 +0900

    game-1

---
+++ Game/Game/Diagram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    internal class Diagram
+    {
+        internal int X
+        {
+            get;
+            private set;
+        }
+        internal int Y
+        {
+            get;
+            private set;
+        }
+    }
+}
