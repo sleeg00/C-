a16fb0812052bf404e65d4504826f730938bffaf
slee00 <slee000220@gmail.com>
2022-05-12 16:07:41 +0900

    스택(1)

---
+++ 스택(백준)/스택(백준)/스택(백준).cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+int s;
+int a[10001];
+void push(int x) {
+	a[s] = x;
+	s++;
+}
+int pop() {
+	if (s == 0)
+		return -1;
+	int t = a[s - 1];
+	s--;
+	return t;
+}
+int size() {
+	return s;
+}
+int top() {
+	if (s == 0) return -1;
+	return a[s - 1];
+}
+int empty() {
+	if (s == 0) return 1;
+	else return 0;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	while (n--) {
+		string str;
+		cin >> str;
+		if (str == "push") {
+			int n;
+			cin >> n;
+			push(n);
+		}
+		if (str == "pop") {
+			cout << pop() << '\n';
+		}
+		if (str == "top") {
+			cout << top() << '\n';
+		}
+		if (str == "size") {
+			cout << size() << '\n';
+		}
+		if (str == "empty") {
+			cout << empty() << '\n';
+		}
+	}
